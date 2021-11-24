98cb94626ce96bd8719244b6bba0e8a1fc79f27d
richardpacheco00 <richard.a.pacheco.iii@gmail.com>
2021-11-23 18:35:54 -0800

    updating it properly :)

--- src/client/SmartAppliance.java
+++ src/client/SmartAppliance.java
@@ -4,6 +4,14 @@ public class SmartAppliance extends Appliance{
 	
 	private int lowW;
 	
+	public SmartAppliance() {
+		super();
+	}
+	
+	public SmartAppliance(String name, int onWattage, int offWattage, double probOn, int location, int ID, int lowWattage){
+		super(name, onWattage, offWattage, probOn, location, ID);
+		lowW = lowWattage;
+	}
 	public void setLowWattage(int w) {
 		lowW = w;
 	}
