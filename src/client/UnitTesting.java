9defdf1c745ce6bf4b45ebc346f70b568aa19ae2
thankfulturtle <90797513+thankfulturtle@users.noreply.github.com>
2021-11-30 22:48:44 -0800

    Update UnitTesting.java

--- src/client/UnitTesting.java
+++ src/client/UnitTesting.java
@@ -1,11 +1,15 @@
 package client;
 
+<<<<<<< Updated upstream
 import java.util.ArrayList;
 
+=======
+>>>>>>> Stashed changes
 public class UnitTesting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+<<<<<<< Updated upstream
 		Scanner scan = new Scanner(System.in);
 		ArrayList<Appliance> applianceList = new ArrayList<Appliance>();
 		
@@ -13,6 +17,8 @@ public class UnitTesting {
 		System.out.println("Enter text file to read: ");
 		appTextFile = 
 		readAppFile()
+=======
+>>>>>>> Stashed changes
 
 	}
 
